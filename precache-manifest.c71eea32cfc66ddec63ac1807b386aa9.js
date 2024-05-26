410a03dfed96c7a06a321bec6c11ed5aa3859c83
MD IRFAN RAJ <mdirfanraj88.omega@gmail.com>
2024-05-26 17:13:12 +0530

    Updates

---
+++ precache-manifest.c71eea32cfc66ddec63ac1807b386aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84287d0a73409cdf47f46c11aaf6730d",
+    "url": "/Text_UTILITY/index.html"
+  },
+  {
+    "revision": "d2dc333115af9f659ee4",
+    "url": "/Text_UTILITY/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "a80683b9983df1994031",
+    "url": "/Text_UTILITY/static/js/2.8924a61f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Text_UTILITY/static/js/2.8924a61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6194039e974543611efe",
+    "url": "/Text_UTILITY/static/js/3.d8d950b7.chunk.js"
+  },
+  {
+    "revision": "d2dc333115af9f659ee4",
+    "url": "/Text_UTILITY/static/js/main.f8b9b146.chunk.js"
+  },
+  {
+    "revision": "bbd6e5f57d36ea09abd9",
+    "url": "/Text_UTILITY/static/js/runtime-main.58c61165.js"
+  }
+]);
